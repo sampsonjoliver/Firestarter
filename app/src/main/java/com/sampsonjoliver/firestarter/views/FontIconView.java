c3d612b41a97927812187a80790b035bd881dba7
sampsonoliveratshine <sampson.oliver@shinetech.com>
2016-07-07 14:44:37 +1000

    Added Material IconFont and FontIconView as replacement for ImageViews containing font icons

---
+++ app/src/main/java/com/sampsonjoliver/firestarter/views/FontIconView.java
@@ -0,0 +1,28 @@
+package com.sampsonjoliver.firestarter.views;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+public class FontIconView extends AppCompatTextView {
+    private static Typeface iconFontTypeface;
+    public static final String iconFontFile = "fonts/MaterialIcons-Regular.ttf";
+
+    public FontIconView(Context context) {
+        this(context, null);
+    }
+
+    public FontIconView(Context context, AttributeSet attrs) {
+        this(context, attrs, android.R.attr.textViewStyle);
+    }
+
+    public FontIconView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        if (iconFontTypeface == null)
+            iconFontTypeface = Typeface.createFromAsset(context.getAssets(), iconFontFile);
+
+        setTypeface(iconFontTypeface);
+    }
+}
