a0853f1fa5da7abffa6ef535fe68f2acf23df855
sampsonoliver <sampson.oliver@shinetech.com>
2016-07-22 23:56:43 +1000

    Added distance, user count, and time text views to collapsible toolbar

--- app/src/main/java/com/sampsonjoliver/firestarter/views/chat/ChannelActivity.kt
+++ app/src/main/java/com/sampsonjoliver/firestarter/views/chat/ChannelActivity.kt
@@ -3,11 +3,13 @@ package com.sampsonjoliver.firestarter.views.chat
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.widget.LinearLayoutManager
+import android.text.format.DateUtils
 import android.util.Log
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.database.*
 import com.sampsonjoliver.firestarter.FirebaseActivity
 import com.sampsonjoliver.firestarter.R
@@ -16,6 +18,8 @@ import com.sampsonjoliver.firestarter.models.Session
 import com.sampsonjoliver.firestarter.service.FirebaseService
 import com.sampsonjoliver.firestarter.service.References
 import com.sampsonjoliver.firestarter.service.SessionManager
+import com.sampsonjoliver.firestarter.utils.DistanceUtils
+import com.sampsonjoliver.firestarter.utils.IntentUtils
 import com.sampsonjoliver.firestarter.utils.TAG
 import com.sampsonjoliver.firestarter.utils.copyToClipboard
 import kotlinx.android.synthetic.main.activity_channel.*
@@ -27,9 +31,20 @@ class ChannelActivity : FirebaseActivity(),
         const val EXTRA_SESSION_ID = "EXTRA_SESSION_ID"
     }
 
-    val session: Session? = null
+    var session: Session? = null
     val sessionId: String? by lazy { intent.getStringExtra(EXTRA_SESSION_ID) }
     val adapter by lazy { ChannelMessageRecyclerAdapter(SessionManager.getUid(), this) }
+
+    val sessionSubscriberListener = object : ValueEventListener {
+        override fun onCancelled(p0: DatabaseError?) {
+            Log.w(this@ChannelActivity.TAG, "onCancelled")
+        }
+
+        override fun onDataChange(p0: DataSnapshot?) {
+            val numSubscribers = p0?.getValue(Int::class.java)
+            users.text = numSubscribers.toString()
+        }
+    }
 
     val sessionListener = object : ValueEventListener {
         override fun onCancelled(p0: DatabaseError?) {
@@ -40,6 +55,14 @@ class ChannelActivity : FirebaseActivity(),
             Log.w(this@ChannelActivity.TAG, "onDataChange: ${p0?.key}")
             val session = p0?.getValue(Session::class.java)
             session?.sessionId = p0?.key
+            this@ChannelActivity.session = session
+
+            banner.setImageURI(session?.bannerUrl)
+            collapsingToolbar.title = session?.topic
+            time.text = DateUtils.formatDateRange(this@ChannelActivity, session?.startDate ?: 0, session?.startDate?.plus(session.durationMs) ?: 0, DateUtils.FORMAT_SHOW_TIME or DateUtils.FORMAT_ABBREV_TIME)
+
+            val geoDistance = DistanceUtils.latLngDistance(session?.location ?: LatLng(0.0, 0.0), LatLng(0.0, 0.0))
+            distance.text = DistanceUtils.formatDistance(geoDistance[0])
         }
     }
 
@@ -97,9 +120,13 @@ class ChannelActivity : FirebaseActivity(),
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         collapsingToolbar.isTitleEnabled = true
-        collapsingToolbar.setTitle("Chatorama") // todo
         toolbar.setOnClickListener { appBarLayout.setExpanded(true, true) }
         messageText.setOnClickListener { appBarLayout.setExpanded(false, true) }
+
+        distance.setOnClickListener {
+            if (session?.location != null)
+                IntentUtils.launchMaps(this@ChannelActivity, session?.location!!, session?.topic)
+        }
 
         recycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, true)
         recycler.adapter = adapter
@@ -153,6 +180,15 @@ class ChannelActivity : FirebaseActivity(),
                 else -> this.addValueEventListener(sessionListener)
             }
         }
+
+        FirebaseService.getReference(References.SessionSubscriptions)
+                .child(sessionId).child("numUsers")
+                .run {
+                    when (detach) {
+                        true -> this.removeEventListener(sessionSubscriberListener)
+                        else -> this.addValueEventListener(sessionSubscriberListener)
+                    }
+                }
     }
 
     override fun onDestroy() {
