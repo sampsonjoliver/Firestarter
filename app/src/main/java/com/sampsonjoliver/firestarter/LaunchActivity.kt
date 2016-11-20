103af46d4b845d05a420a3931a895c33bdddf808
sampsonoliver <sampsonjoliver@outlook.com>
2016-11-20 14:59:00 +1100

    Added location permission checks to splash screen LaunchActivity

--- app/src/main/java/com/sampsonjoliver/firestarter/LaunchActivity.kt
+++ app/src/main/java/com/sampsonjoliver/firestarter/LaunchActivity.kt
@@ -1,8 +1,14 @@
 package com.sampsonjoliver.firestarter
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Build
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import com.sampsonjoliver.firestarter.views.main.HomeActivity
 
 class LaunchActivity : FirebaseActivity() {
@@ -11,11 +17,53 @@ class LaunchActivity : FirebaseActivity() {
         setContentView(R.layout.activity_launch)
     }
 
-    override fun onLogin() {
+    fun checkLocationPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an explanation and handle successes and failures
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle(getString(R.string.location_permission_rationale_title))
+                        .setMessage(getString(R.string.location_permission_rationale_message))
+                        .setNegativeButton(getString(R.string.cancel), { dialogInterface, i ->
+                            dialogInterface.dismiss()
+                        })
+                        .setPositiveButton(getString(R.string.grant), { dialogInterface, i ->
+                            dialogInterface.dismiss()
+                            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LocationAwareActivity.PERMISSION_REQUEST_FINE_LOCATION)
+                        }).show()
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LocationAwareActivity.PERMISSION_REQUEST_FINE_LOCATION)
+            }
+        } else {
+            // Permissions granted, so lets continue
+            startHomeActivity()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            LocationAwareActivity.PERMISSION_REQUEST_FINE_LOCATION -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.getOrNull(0) == PackageManager.PERMISSION_GRANTED) {
+                    startHomeActivity()
+                } else {
+                    finish()
+                }
+            }
+        }
+    }
+
+    fun startHomeActivity() {
         startActivity(Intent(this, HomeActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
                 addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
         })
+    }
+
+    override fun onLogin() {
+        checkLocationPermissions()
     }
 }
